2a3b9ed2b7833372e4dc818e7811256e72d45fde
gbushong <graceebushong@gmail.com>
2016-04-22 12:12:36 -0400

    Frequencies added to record.cpp

--- record.cpp
+++ record.cpp
@@ -22,23 +22,23 @@ Record::Record(){
 		exit(1);
 	}
 	//add frequencies
-	Notes["a"]=1;
-	Notes["s"]=1;
-	Notes["d"]=2;
-	Notes["f"]=;
-	Notes["g"]=;
-	Notes["h"]=;
-	Notes["j"]=;
-	Notes["k"]=;
-	Notes["l"]=;
-	Notes[";"]=;
-	Notes["w"]=;
-	Notes["e"]=;
-	Notes["t"]=;
-	Notes["y"]=;
-	Notes["u"]=;
-	Notes["o"]=;
-	Notes["p"]=;
+	Notes["a"]=261.63;   // Middle C (C4)
+	Notes["s"]=293.66;   // D4
+	Notes["d"]=329.63;   // E4
+	Notes["f"]=349.23;    // F4
+	Notes["g"]=392.00;    // G4
+	Notes["h"]=440.00;    // A4 (tuning pitch)
+	Notes["j"]=493.88;    // B4
+	Notes["k"]=523.25;    // C5
+	Notes["l"]=587.33;    // D5
+	Notes[";"]=659.25;    // E5
+	Notes["w"]=277.18;    // C#4/ Db4
+	Notes["e"]=311.13;    // D#4/ Eb4
+	Notes["t"]=369.99;    // F#4/ Gb4
+	Notes["y"]=415.30;    // G#4/ Ab4
+	Notes["u"]=466.16;    // A#4/ Bb4
+	Notes["o"]=554.37;    // C#5/ Db5
+	Notes["p"]=622.25;    // D#5/ Eb5
 }
 
 //add key to the vector Recorded
