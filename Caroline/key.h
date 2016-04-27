9a686916d29f57c5e76f15517f76823ba32e26d6
cbraun1 <caroline@mypicante.com>
2016-04-26 23:13:52 -0400

    comments

--- Caroline/key.h
+++ Caroline/key.h
@@ -1,3 +1,7 @@
+/*
+header file containing the class definition and functions for the key class
+*/
+
 #ifndef key_H
 #define key_H
 
@@ -7,36 +11,40 @@
 #include <string>
 #include <cmath>
 
-class Key {
+class Key { //key class
  public:
-  Key();
-  void drawOneKey(SDL_Renderer*);
-  void colorKey(SDL_Renderer *);
-  void setVals(int,int);
+  Key(); //default constructor
+  void drawOneKey(SDL_Renderer*); //function draws one key
+  void colorKey(SDL_Renderer *); //function colors one key
+  void setVals(int,int); //function sets x position and y position of key
  private:
-  const int keyWid = 60;
-  const int keyLen = 450;
-  int xpos;
-  int ypos;
+  const int keyWid = 60; //variable storing width of one key
+  const int keyLen = 450; //variable storing length of one key
+  int xpos; //variable storing x position of key
+  int ypos; //variable storing y position of key
 };
 
+//default constructor
 Key::Key(){
 }
 
+//function sets x position and y position of key
 void Key::setVals(int xval, int yval){
 	xpos=xval;
 	ypos=yval;
 }
 
+//function draws one key
 void Key::drawOneKey(SDL_Renderer* gRenderer){
-  SDL_Rect pianoKey = {xpos, ypos, keyWid, keyLen};
-  SDL_SetRenderDrawColor(gRenderer, 0, 0, 100, 100);
-  SDL_RenderDrawRect(gRenderer, &pianoKey);
+  SDL_Rect pianoKey = {xpos, ypos, keyWid, keyLen}; //creates rectangle in SDL
+  SDL_SetRenderDrawColor(gRenderer, 0, 0, 100, 100); //changes color of rectangle
+  SDL_RenderDrawRect(gRenderer, &pianoKey); //renders rectangle to screen
 }
 
+//function colors one key
 void Key::colorKey(SDL_Renderer* gRenderer){
-  SDL_Rect color = {xpos, ypos, keyWid, keyLen};
-  SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-  SDL_RenderFillRect(gRenderer, &color);
+  SDL_Rect color = {xpos, ypos, keyWid, keyLen}; //creates rectangle in SDL
+  SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF); //changes color of rectangle
+  SDL_RenderFillRect(gRenderer, &color); //renders rectangle to screen
 }
 #endif
